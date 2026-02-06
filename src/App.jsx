278d712dd0bfa188293fe1da734acd926623ac7f
hong203 <nguyenngochong03@gmail.com>
2026-02-06 19:41:16 +0700

    Revert "Remove unused nextTab and prevTab functions that referenced undefined setCurrentTab"
    This reverts commit 5fa35330b2eaa3fe2c9e3ffee84265b821f14a44.

--- src/App.jsx
+++ src/App.jsx
@@ -50,6 +50,14 @@ const App = () => {
     ]
   }
 
+  const nextTab = () => {
+    setCurrentTab((prev) => (prev + 1) % 3)
+  }
+
+  const prevTab = () => {
+    setCurrentTab((prev) => (prev - 1 + 3) % 3)
+  }
+
   return (
     <div className="app">
       <div className="frame">
